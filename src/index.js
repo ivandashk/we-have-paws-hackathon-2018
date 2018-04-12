3630598c00b4939a1281d3d6be45475a448f8270
mrnexeon <the.nexeon@gmail.com>
2018-04-12 22:51:51 +0300

    npm init

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import App from './Components/App.js'
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+);
---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+var path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+          { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+        ]
+      }
+};
