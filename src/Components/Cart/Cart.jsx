42c2cd62a593b018dd66ef40806ca546795bb79d
Ivan Dashkevich <idashkevich@lement.pro>
2018-04-25 00:06:12 +0300

    Place-only-demo added

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import './Cart.css';
 
 class Cart extends Component {
+    constructor(props) { 
+        super(props); 
+        this.observer = props.observer;
+    }
+
     render() {
         return (
-            <div>
-                <h1>Корзина</h1>
+            <div className='cart-container'>
+                <div className='cart-header'>Корзина</div>
                 <ul>
                     <li>
                         <span>A1, Ряд 6, Место 9, 1500 р.</span>
---
+++ webpack-configs/place-only.config.js
@@ -0,0 +1,36 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/place-only-demo.js',
+    output: {
+        path: path.resolve(__dirname, './../public/place-only-demo'),
+        filename: 'bundle.js'
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+           $: "jquery",
+           jQuery: "jquery"
+	   	})
+	],
+    module: {
+        rules: [
+            { test: /\.js$/, exclude: [/node_modules/, /public/], loader: "babel-loader" },
+            { test: /\.css$/, exclude: [/node_modules/, /public/], loader: "style-loader!css-loader" },
+			{
+				test: /\.svg$/,
+				loader: "url-loader?limit=10000&mimetype=image/svg",
+			},
+            {
+                test: /\.jsx$/,
+                loader: "babel-loader",
+                exclude: [/node_modules/, /public/], 
+				query:
+				  {
+					presets:['react']
+				  }
+            },
+        ]
+      }
+};
