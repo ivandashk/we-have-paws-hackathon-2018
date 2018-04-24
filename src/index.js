05e1d43f0b0716ed028bf74f0f5ab7b040f8dc30
Ivan Dashkevich <idashkevich@lement.pro>
2018-04-25 00:06:12 +0300

    Place-only-demo added

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import style from './styles/style.css';
-import App from './Components/App'
-
-ReactDOM.render(
-    <App />,
-    document.getElementById('root')
-);
